9068d332d7ccaf6d71fe7ce562c1bffe1c835687
Evan Murray <evanmurray@localhost.localdomain>
2019-02-07 19:31:56 -0500

    modified comments in answers.txt and added palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+_my_list = []
+def main():
+	fh = open('cities.txt')
+	for line in fh:
+		line = line.strip()
+		reverse = line[::-1]
+		if line.lower() == reverse.lower():
+			print(line)
+	fh.close()
+if __name__ == '__main__':
+	main()
